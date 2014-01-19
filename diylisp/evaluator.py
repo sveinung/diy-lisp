cd96b53c18d3c6795fbdef221f0089ebaf64b173
Sveinung Dalatun <sdalatun@gmail.com>
2014-01-19 15:39:12 +0100

    Test 2 ferdig

--- diylisp/evaluator.py
+++ diylisp/evaluator.py
@@ -6,6 +6,7 @@ from types import Lambda
 from types import is_boolean, is_atom, is_symbol, is_list, is_lambda, is_integer
 from asserts import assert_exp_length, assert_valid_definition, assert_boolean
 from parser import unparse
+import operator as op
 
 def evaluate(ast, env):
     """Evaluate an Abstract Syntax Tree in the specified environment."""
@@ -16,12 +17,26 @@ def evaluate(ast, env):
     if ast[0] == "atom":
         return atom(ast[1], env)
     if ast[0] == "eq":
-        print(ast)
         first = evaluate(ast[1], env)
         second = evaluate(ast[2], env)
         return first == second
+    if ast[0] == "+":
+        return do_math(ast, op.add, env)
+    if ast[0] == "-":
+        return do_math(ast, op.sub, env)
+    if ast[0] == "/":
+        return do_math(ast, op.div, env)
+    if ast[0] == "*":
+        return do_math(ast, op.mul, env)
+    if ast[0] == "mod":
+        return do_math(ast, op.mod, env)
+    if ast[0] == ">":
+        return do_math(ast, op.gt, env)
     else:
         raise NotImplementedError(ast)
 
 def atom(exp, env):
     return not is_list(evaluate(exp, env))
+
+def do_math(ast, operator, env):
+    return operator(evaluate(ast[1], env), evaluate(ast[2], env))
